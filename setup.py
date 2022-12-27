38961aeabeb16652573d13eb964f013ecbb3f648
Peng Zan <zanpeng.pz@gmail.com>
2022-12-26 23:40:43 -0500

    * add pre-requisites to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,23 @@
 from setuptools import setup, find_packages
 
-setup(name='embrain', version='0.1', packages=find_packages())
+setup(name='embrain', version='0.1', packages=find_packages(),
+      install_requires=[
+        'PyYAML',
+        'flatbuffers==2.0.7',
+        'tensorflow_addons',
+        'protobuf==3.19',
+        'onnx',
+        'onnxruntime-gpu',
+        'torch',
+        'tf2onnx',
+        'onnxconverter-common',
+        'scikit-learn==1.1.1',
+        'scipy',
+        'skl2onnx',
+        'tflite',
+        'tensorflow-gpu', 
+        'rich',
+        'onnx-simplifier', 
+        'onnx-tf',
+        'tensorflow-probability'
+      ])
